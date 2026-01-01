95ab909795fc08786a602e67b9ffa0e8fe7b1948
sajidmahamud835 <sajidmahamud835@gmail.com>
2026-01-02 05:16:53 +0600

    refactor: Install Bootstrap via npm instead of CDN
    - Install bootstrap@5.3.3 as npm dependency
    - Import Bootstrap CSS and JS in index.jsx
    - Remove CDN links from index.html
    - Better bundling and tree-shaking support

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+// Bootstrap CSS and JS (npm package)
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
